ee25096dd8abf00f68c0534067e0383ed7c36b49
AsmitBhardwaj <asmitbhardwaj1@gmail.com>
2026-04-03 02:54:10 -0400

    doing logic of guessed states

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,11 +8,28 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-#mouse coordinates
-def get_mouse_coor(x,y):
-    print(x,y)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-turtle.onscreenclick(get_mouse_coor)
-turtle.mainloop()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title = f"{len(guessed_states)}/50 States correct", prompt="What's another state's name?").title() 
+    print(answer_state)
 
-screen.exitonclick()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")      
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
